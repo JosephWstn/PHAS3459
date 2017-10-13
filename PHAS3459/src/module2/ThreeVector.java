e3b8d5e329af837bb2363bded9f913e28b5b2563
zcapjdw <zcapjdw@CT00006684.ad.ucl.ac.uk>
2017-10-13 11:31:40 +0100

    Module 2. About to alter public methods that call static methods.

--- PHAS3459/src/module2/ThreeVector.java
+++ PHAS3459/src/module2/ThreeVector.java
@@ -21,37 +21,76 @@ public class ThreeVector {
 
 
 	public ThreeVector unitVector() { //find unit vector of a vector
-		ThreeVector V = new ThreeVector(x, y, z);
-		ThreeVector UV = new ThreeVector(x/V.magnitude(), y/V.magnitude(), z/V.magnitude() ); //yaknow mathsy stuff
+		ThreeVector UV = new ThreeVector(x/this.magnitude(), y/this.magnitude(), z/this.magnitude() ); //yaknow mathsy stuff
 		return UV;
 	}
 
 	public String toString(){ //converts my "object" to a string. idk what the fuck my object is
 		return "("+x+","+y+","+z+")";
 	}
-
-//THIS BIT 
+	
+	
 	static double scalarProduct(ThreeVector A, ThreeVector B){
-
-		
-		double E = (A.x* B.x + A.y *B.y + A.z * B.z);
-		return E;
+		double SP = (A.x* B.x + A.y *B.y + A.z * B.z);
+		return SP;
 	}
 	
+	
+	static ThreeVector vectorProduct(ThreeVector A, ThreeVector B) {
+		double NX = A.y * B.z - A.z * B.y;
+		double NY = A.x * B.z - A.z * B.x;
+		double NZ = A.x * B.y - A.y * B.x;
+		ThreeVector VP = new ThreeVector (NX , -NY, NZ);
+		return VP;
+		
+	}
+	
+	static ThreeVector add (ThreeVector A, ThreeVector B) {
+		ThreeVector a = new ThreeVector (A.x + B.x, A.y + B.y, A.z + B.z);
+		return a;
+		
+	}
+	
+	static double angle (ThreeVector A, ThreeVector B) {
+		double cosang = scalarProduct(A,B) / (A.magnitude() * B.magnitude());
+		return Math.acos(cosang);
+	}
+	
+	public double scalarProduct2(ThreeVector A, ThreeVector B){
+		double SP2 = scalarProduct(A,B);
+		return SP2;
+	}
 	 
+	public ThreeVector vectorProduct2(ThreeVector A, ThreeVector B){
+		ThreeVector VP2 = vectorProduct(A,B);
+		return VP2;
+	}
+	
+	
+	public ThreeVector add2 (ThreeVector A, ThreeVector B){
+		ThreeVector a2 = add(A,B); 
+		return a2;
+	}
+	
+	public double angle2(ThreeVector A, ThreeVector B) {
+		double ang2 = angle(A,B);
+		return ang2;
+	}
+	
 	public static void main(String[] args) {
 
 		ThreeVector tv = new ThreeVector(1.0,2.0,3.0); //calling a vector (1,2,3)
 		ThreeVector tvt = new ThreeVector(4.0,5.0,6.0);
+		
 		double mag = tv.magnitude(); //do find magnitude of (1,2,3)
 		ThreeVector unitv = tv.unitVector(); // find unitvector of (1,2,3)
-		
 		double scalar = scalarProduct(tv,tvt);
+		ThreeVector cross = vectorProduct(tv,tvt);
+		ThreeVector addition = add(tv,tvt);
+		double angle = angle(tv,tvt);
 		
-		//System.out.print(unitv); //print dat shit
-		//System.out.println(mag);
-		//System.out.println(tv);
-		System.out.println(scalar);
+
+		System.out.println(angle);
 	}
 
 }
