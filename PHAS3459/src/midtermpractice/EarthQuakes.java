64fa76e45768a3c20dba21b0c62bd9302b6ad5e8
zcapjdw <zcapjdw@ucl.ac.uk>
2017-11-14 13:25:05 +0000

    Details of max earthquake found

--- PHAS3459/src/midtermpractice/EarthQuakes.java
+++ PHAS3459/src/midtermpractice/EarthQuakes.java
@@ -14,49 +14,67 @@ import module5.DataPoint;
 
 public class EarthQuakes {
 
-	public static String dataAnalysis(String urlName) throws Exception {
+	public static BufferedReader dataAsBR(String urlName) throws Exception {
 		URL u = new URL (urlName);
 		InputStream is = u.openStream();
 		InputStreamReader isr = new InputStreamReader(is);
-		BufferedReader br = new BufferedReader(isr);	
-		
-		double max = 0;
+		return new BufferedReader(isr);	
+	}
+
+	public static String maxdata(BufferedReader br) throws Exception {
+		int maximumid =0;
+		double maxmag =0, maxsec=0, maxlat=0, maxlong=0, maxdep=0, maxmaj=0, maxmin=0, maxaz=0, maxzerr = 0;
+		int tot=0, maxmonth=0, maxyear = 0, maxday=0, maxminute=0, maxhour=0;
 		String line;
 		while ((line = br.readLine()) != null) {
-			
-			Scanner s = new Scanner(line);
-			
-			String year = s.next();
-			/*
-			double month = s.nextDouble();
-			double day = s.nextDouble();
-			double hour = s.nextDouble();
-			double minute = s.nextDouble();
-			double second = s.nextDouble();
-			double latitude = s.nextDouble();
-			double longitude = s.nextDouble();
-			double depth = s.nextDouble();
-			double major = s.nextDouble();
-			double minor = s.nextDouble();
-			double azimuth = s.nextDouble();
-			double zerr = s.nextDouble();
-			
-			double mag = s.nextDouble();
-			
-			if(mag > max) {
-				max = mag;
-				int maximumid = s.nextInt();
+			if ( !Character.isDigit(line.charAt(0))){
 			}
 			else {
-				int id = s.nextInt();
+				Scanner s = new Scanner(line);
+				tot++;
+				int year =  s.nextInt();
+				int month =  s.nextInt();
+				int day = s.nextInt();
+				int hour = s.nextInt();
+				int minute = s.nextInt();
+				double second = s.nextDouble();
+				double latitude = s.nextDouble();
+				double longitude = s.nextDouble();
+				double depth = s.nextDouble();
+				double major = s.nextDouble();
+				double minor = s.nextDouble();
+				double azimuth = s.nextDouble();
+				double zerr = s.nextDouble();
+
+				double mag = s.nextDouble();
+
+				if(mag > maxmag) {
+					maxmag = mag;
+					maximumid = s.nextInt();
+					maxyear = year;
+					maxmonth = month;
+					maxday = day;
+					maxhour = hour;
+					maxminute = minute;
+					maxsec = second;
+					maxlat = latitude;
+					maxlong = longitude;
+					maxdep = depth;
+					maxmaj = major;
+					maxmin = minor;
+					maxaz = azimuth;
+					maxzerr = zerr;
+				}
+				else {
+					int id = s.nextInt();
+				}
+
+				s.close();
+
 			}
-			*/
-			s.close();
-			return year;
 		}
-		return "";
+		return( "Total number of Eathquakes is: " + tot + "\n" + "Maximum earthquake magnitude is: " + maxmag + ". ID of this is " + maximumid + ". This Eathquake happened on " + maxday +" / " + maxmonth + " / " + maxyear +" at time " + maxhour + ":"+ maxminute + ":"+ maxsec +".\nThe latitude was " + maxlat + " and the longitude was "+maxlong + ". \nIt occured at a depth of " + maxdep + " with an uncertainty of " + maxzerr + ". \nThe major and minor axis (km) and azimuth of 95% relative location are " + maxmaj +", "+ maxmin +", "+ maxaz + " respectively.");
 	}
-	
 
 	/*
 	public static HashMap<Integer, Double> idmag(BufferedReader br) throws IOException {
@@ -93,46 +111,10 @@ public class EarthQuakes {
 	}
 	 */
 
-	/*
-	public static double maxmag (BufferedReader br) throws IOException {
-		//buffered reader to scanner
-		double max = 0;
-		int maximumid = 0;
-		String line;
-		while ((line = br.readLine()) != null) {
-			
-			Scanner s = new Scanner(line);
-			double year = s.nextDouble();
-			double month = s.nextDouble();
-			double day = s.nextDouble();
-			double hour = s.nextDouble();
-			double minute = s.nextDouble();
-			double second = s.nextDouble();
-			double latitude = s.nextDouble();
-			double longitude = s.nextDouble();
-			double depth = s.nextDouble();
-			double major = s.nextDouble();
-			double minor = s.nextDouble();
-			double azimuth = s.nextDouble();
-			double zerr = s.nextDouble();
-			double mag = s.nextDouble();
-			if(mag > max) {
-				max = mag;
-				maximumid = s.nextInt();
-			}
-			else {
-				int id = s.nextInt();
-			}
-			s.close();
-		}
-		return max;
-	}
-*/
-
 	public static void main(String[] args) throws Exception {
 
-		String maxmag = dataAnalysis("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2015-16/earthquakesCA1989.txt");
-		System.out.println(maxmag);
+		BufferedReader brdata = dataAsBR("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2015-16/earthquakesCA1989.txt");
+		System.out.println(maxdata(brdata));
 
 	}
 }
