df2bd4104127d372b40abeb966b196b48a645186
zcapjdw <zcapjdw@ucl.ac.uk>
2018-01-10 13:52:57 +0000

    12/13 i think txt file is broken

--- PHAS3459/src/finalexam1213/ExamPart11.java
+++ PHAS3459/src/finalexam1213/ExamPart11.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class ExamPart11 {
@@ -35,6 +36,53 @@ public class ExamPart11 {
 		return list;
 	}
 
+	//make an arraylist of all of the specimen data
+	public static HashMap<String, ArrayList<Double>> energyMapFromURL(String urlName) throws IOException{
+
+		//initialise the arraylists of energy for two IDs	
+		ArrayList<Double> GGList = new ArrayList<Double>();
+		ArrayList<Double> ZZList = new ArrayList<Double>();
+
+		//initialise HashMap
+		HashMap<String, ArrayList<Double>> energyMap = new HashMap<String, ArrayList<Double>>();
+
+		//read the URL and set it to a buffered reader
+		URL u = new URL(urlName);
+		InputStream is = u.openStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		String line;
+
+		//loop through text adding energy to the two arraylists
+
+		while ((line = br.readLine()) != null){
+			Higgs currentLine = new Higgs(line);
+
+			if(line.contains("Channel")) {
+				System.out.println("title");
+			}
+
+			else if(currentLine.getID() == "GG \\s") {
+				GGList.add(currentLine.getEnergy());
+				System.out.println("GGadd");
+			}
+			
+			else if(currentLine.getID()=="ZZ \\s") {
+				System.out.println("ZZadd");
+				ZZList.add(currentLine.getEnergy());
+			}
+			else {
+				//System.out.println("failed: "+currentLine.getID()+"\n"+currentLine.getEnergy()+"\n\n");
+			}
+		}
+
+		//put everything into the map
+		energyMap.put("GG", GGList);
+		energyMap.put("ZZ", ZZList);
+
+		return energyMap;
+	}
+
 	//calculates number of readings within a certain range
 	public static double readingsInRange (ArrayList<Background> list, int min, int max) {
 		int currentMin, currentMax;
@@ -52,9 +100,14 @@ public class ExamPart11 {
 
 
 	public static void main(String[] args) throws IOException {
-		
-		ArrayList<Background> list = listFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2011-12/backgroundZZ.txt");
-		System.out.println(readingsInRange(list, 120, 140));
+
+		ArrayList<Background> listZZ = listFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2011-12/backgroundZZ.txt");
+		ArrayList<Background> listGG = listFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2011-12/backgroundGG.txt");
+		HashMap<String, ArrayList<Double>> higgsMap = energyMapFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/2011-12/higgsData.txt");
+		System.out.println("Expected readings between 120 and 140 for ZZ: "+readingsInRange(listZZ, 120, 140));
+		System.out.println("Expected readings between 120 and 140 for GG: "+readingsInRange(listGG, 120, 140));
+		System.out.println("Number of protential GG higgs: "+ higgsMap.get("GG").size());
+		System.out.println("Number of protential ZZ higgs: "+ higgsMap.get("ZZ").size());
 	}
 
 }
--- PHAS3459/src/finalexam1213/Higgs.java
+++ PHAS3459/src/finalexam1213/Higgs.java
@@ -10,16 +10,21 @@ public class Higgs {
 	public Higgs(){}
 	public Higgs(String line){
 		Scanner sc = new Scanner(line);
+
 		this.id = sc.next();
-		this.energy = sc.nextDouble();
+		this.energy=sc.nextDouble();
+
 		sc.close();
+
+
+
 	}
-	
+
 	//getters
 	public String getID() {
 		return this.id;
 	}
-	
+
 	public double getEnergy() {
 		return this.energy;
 	}
